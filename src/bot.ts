c3af1128cf19aecdc794180d191724b8a0f28658
xg4 <xingor4@gmail.com>
2021-03-11 14:15:23 +0800

    chore: update

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,47 @@
 import { Telegraf } from "telegraf";
 import { BOT_TOKEN, SECRET_PATH, WEBHOOK } from "./config";
+import { userController } from "./controllers";
 
 export const bot = new Telegraf(BOT_TOKEN);
+
+bot.start(userController.token);
+
+bot.command("token", userController.token);
+
+bot.command("revoke", userController.revoke);
+
+bot.command("show", userController.show);
+
+bot.command("image", (ctx) =>
+  ctx.replyWithPhoto({ url: "https://picsum.photos/200/300/?random" })
+);
+
+bot.help((ctx) => ctx.reply(`For more info, see: `));
+
+const commands = [
+  {
+    command: "token",
+    description: "generate authorization token",
+  },
+  {
+    command: "revoke",
+    description: "revoke access token",
+  },
+  {
+    command: "show",
+    description: "show your current token",
+  },
+  {
+    command: "image",
+    description: "get a random image",
+  },
+  {
+    command: "help",
+    description: "show help message",
+  },
+];
+
+bot.telegram.setMyCommands(commands);
 
 bot.catch((err: any) => {
   console.error("[bot] err:", err);
