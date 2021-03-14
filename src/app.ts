4fe1a0c0f31975340d70097ffca33602697f4fee
xg4 <xingor4@gmail.com>
2021-03-14 16:05:11 +0000

    chore: add next.js

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import next from "next";
+import { join } from "path";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { bot } from "./bot";
@@ -9,19 +11,32 @@ import { router } from "./routes";
 
 const port = process.env.PORT || 3000;
 
-createConnection()
-  .then(() => {
-    const app = express();
-    app.use(cors());
-    app.use(express.json());
-    app.use(morgan("tiny"));
+const app = next({ dev: true, dir: join(__dirname, "../client") });
+const handle = app.getRequestHandler();
 
-    app.use(router);
+async function main() {
+  await app.prepare();
+  // connect database
+  await createConnection();
 
-    app.use(SECRET_PATH, (req, res) => bot.handleUpdate(req.body, res));
+  const server = express();
 
-    app.listen(port, () => {
-      console.log(`http://localhost:${port}`);
-    });
-  })
-  .catch(console.error);
+  // middleware
+  server.use(cors());
+  server.use(express.json());
+  server.use(morgan("tiny", { skip: (req) => req.url.startsWith("/_next") }));
+
+  // router
+  server.use(router);
+  // telegram bot webhook, telegraf bot
+  server.use(SECRET_PATH, (req, res) => bot.handleUpdate(req.body, res));
+
+  // client, next.js
+  server.all("*", (req, res) => handle(req, res));
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+}
+
+main();
