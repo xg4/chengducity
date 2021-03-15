cf0961afd4b3e0ad1fb7cd5a308c93f552fd914e
xg4 <xingor4@gmail.com>
2021-03-15 20:58:32 +0800

    chore: update ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,20 @@
+const url = process.env.DATABASE_URL;
+const dbConfig = url
+  ? {
+      url,
+    }
+  : {
+      host: process.env.DB_URL || 'db',
+      port: process.env.DB_PORT || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'postgres',
+    };
+
 /** @type {import('typeorm').ConnectionOptions} */
 module.exports = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  ...dbConfig,
   synchronize: false,
   logging: process.env.NODE_ENV !== 'production',
   ssl: process.env.IS_HEROKU
