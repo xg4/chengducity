0d0dafb13711d1edda6302898606196387ba87c6
xg4 <xingor4@gmail.com>
2021-04-19 10:25:12 +0800

    chore: update deps

--- server/routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { houseController } from './controllers';
-
-export const router = express.Router();
-
-router.post('/pull', houseController.pull);
-
-router.get('/year/:year', houseController.year);
